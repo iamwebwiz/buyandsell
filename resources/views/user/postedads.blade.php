1d1359d5288f602a36337406092de3a5dabfb186
iamwebwiz <iamwebwiz@gmail.com>
2017-08-16 03:41:24 +0100

    modified

--- resources/views/user/postedads.blade.php
+++ resources/views/user/postedads.blade.php
@@ -14,7 +14,22 @@ Posted Ads
 		<div class="panel-body">
 			<h3 class="thin">Adverts posted by you</h3>
 			<hr>
-			{{ $user->firstname }}
+			<table class="table table-hover">
+				<thead>
+					<td></td>
+					<td>Title</td>
+					<td>Location</td>
+					<td>Phone</td>
+					<td>Action</td>
+				</thead>
+				<tbody>
+					<td></td>
+					<td>Flight booking ticket</td>
+					<td>Magboro, Lagos</td>
+					<td>08163947019</td>
+					<td><a href="#" class="btn btn-danger">Delete</a></td>
+				</tbody>
+			</table>
 		</div>
 	</div>
 </div>
